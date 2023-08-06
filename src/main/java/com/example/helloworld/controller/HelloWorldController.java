fd83090707240f9fa0b6e6dba28be65004184d20
burhantaskesen <taskesenburhanoff@gmail.com>
2023-08-06 13:16:54 +0300

    Initial commit

---
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok("Hello world !");
+    }
+}
